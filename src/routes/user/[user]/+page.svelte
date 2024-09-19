7748a0bd9ae2d46ae739f95f25e5a580e7f1e637
The Legendary John <legendaryjohn2005@gmail.com>
2024-09-19 20:40:59 +0700

    [feat] Implemented User page (unfinished)

--- src/routes/user/[user]/+page.svelte
+++ src/routes/user/[user]/+page.svelte
@@ -1,84 +1,72 @@
 <script>
+	import './user.scss';
 	export let data;
 </script>
 
-<title>ENTREE | User Profile | {data.username}</title>
+<title>ENTREE | Profile | {data.username}</title>
 
 <!-- Home page container -->
-<div id="home-page">
+<div id="user-page">
 	<!-- Signin section -->
-	<section id="signin-section">
+	<section id="avatar-section">
 		<!-- Decor rectangles -->
 		<div class="decor-rectangle-2 decor-rectangle" />
 		<div class="decor-rectangle-1 decor-rectangle" />
 
 		<!-- Signin content -->
-		<div id="signin-content">
-			<h1 id="signin-title">COLLABORATION<br />BRINGS INNOVATION.</h1>
-			<p id="signin-desc">Connect with other entrepreneurs, now easier than ever<br />before.</p>
-			<div id="signin-btn-container">
-				<button class="signup-btn signin-btn">Sign Up</button>
-				<button class="login-btn signin-btn"> Log In</button>
+		<div id="avatar-content">
+			<div id="avatar-img-container" class="box-shadow">
+				<img src={data.avatar} alt={data.username} id="avatar-img" />
 			</div>
-		</div>
 
-		<!-- Signin image -->
-		<div id="signin-img-container" class="box-shadow">
-			<img src="https://picsum.photos/700/700" alt="" id="signin-img" />
-		</div>
-	</section>
-
-	<!-- Intro section 1 -->
-	<section id="intro-container-1">
-		<div class="decor-square-container">
-			<div class="decor-square-1 decor-square" />
-			<div class="decor-square-2 decor-square" />
-		</div>
-
-		<div class="intro-content">
-			<h1 class="intro-title">Find what matters to you. Fast.</h1>
-			<p class="intro-desc">
-				With Entree, lorem ispum dolar sit amet. Lorem ispum dolar sit amet Lorem ispum dolar sit
-				amet Lorem ispum dolar sit amet Lorem ispum dolar sit amet Lorem ispum dolar sit amet Lorem
-				ispum dolar sit amet Lorem ispum dolar sit amet
-			</p>
-		</div>
-	</section>
-
-	<!-- Intro section 2 -->
-	<section id="intro-container-2">
-		<div class="decor-square-container">
-			<div class="decor-square-1 decor-square" />
-			<div class="decor-square-2 decor-square" />
-			<div class="decor-square-3 decor-square" />
-			<div class="decor-square-4 decor-square" />
-			<div class="decor-square-5 decor-square" />
-		</div>
-		<div class="intro-content">
-			<h1 class="intro-title">Startups made more accessible than ever before.</h1>
-			<p class="intro-desc">
-				With Entre, lorem ispum dolar sit amet. Lorem ispum dolar sit amet Lorem ispum dolar sit
-				amet Lorem ispum dolar sit amet Lorem ispum dolar sit amet Lorem ispum dolar sit amet Lorem
-				ispum dolar sit amet Lorem ispum dolar sit amet Lorem ispum dolar sit amet Lorem ispum dolar
-				sit amet Lorem ispum dolar sit amet
-			</p>
-		</div>
-	</section>
-
-	<!-- Invite section -->
-	<section id="invite-container">
-		<div id="invite-content-container">
-			<h1 id="invite-title">What are you waiting for?</h1>
-		</div>
-		<div id="invite-btn-container">
-			<div id="invite-btn-background">
-				<div id="invite-btn-title-container">
-					<h1 id="invite-btn-title">Start now.</h1>
-				</div>
-				<div id="signup-btn-container">
-					<button class="signup-btn">Signup</button>
+			<div id="avatar-info">
+				<h1 id="avatar-name">{data.username}</h1>
+				<h2 id="avatar-age">{data.age}</h2>
+				<div id="avatar-title-container">
+					{#each data.title as title}
+						<div id="avatar-title-background">
+							<p id="avatar-title">{title}</p>
+						</div>
+					{/each}
 				</div>
 			</div>
+		</div>
+	</section>
+
+	<!-- Bio section -->
+	<section id="bio-section">
+		<div id="bio-about">
+			<h1 id="bio-about-title">About me</h1>
+			<p id="bio-about-desc">{data.about}</p>
+		</div>
+
+		<div id="bio-interests">
+			<h1 id="bio-interests-title">Interests</h1>
+			<div id="bio-interests-container">
+				{#each data.interests as interest}
+					<div id="bio-interest-background">
+						<p id="bio-interest">{interest}</p>
+					</div>
+				{/each}
+			</div>
+		</div>
+	</section>
+
+	<!-- Projects Section -->
+	<section id="projects-section">
+		<h1 id="projects-title">My Projects</h1>
+		<div id="projects-grid">
+			{#each data.projects as project}
+				<div class="project-card box-shadow">
+					<img
+						class="project-thumbnail"
+						src={project.image || 'https://picsum.photos/700/700'}
+						alt={project.title}
+					/>
+					<h2 class="project-title">{project.title}</h2>
+					<p class="project-description">{project.description}</p>
+				</div>
+			{/each}
 		</div>
 	</section>
 </div>
