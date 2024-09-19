bf6901f1298debd26dbbfd5e983862df09dfae03
The Legendary John <legendaryjohn2005@gmail.com>
2024-09-20 01:06:47 +0700

    [initial] Initial commit for Contacts page

--- src/routes/contacts/+page.svelte
+++ src/routes/contacts/+page.svelte
@@ -1,4 +1,90 @@
 <script>
+	import './contacts.scss';
 </script>
 
 <title>ENTREE | Contacts</title>
+
+<!-- Contacts page container -->
+<div id="contacts-page">
+	<!-- About section -->
+	<section id="about-section">
+		<!-- About content -->
+		<div id="about-content">
+			<h1 id="about-title">About Us</h1>
+			<p id="about-desc">
+				At <b style="color: var(--accent-brightorange);">ENTREE</b>, we believe in empowering
+				communities and individuals through innovative solutions. Our mission is to drive positive
+				change by providing accessible tools and resources that inspire action, growth, and
+				collaboration. Whether through technology, education, or support, we are dedicated to making
+				a difference in the world around us. Join us on our journey to create a brighter, more
+				connected future.
+			</p>
+		</div>
+
+		<!-- About images -->
+		<div id="about-img-section">
+			<div id="about-img-container">
+				<div id="about-img-background-1">
+					<img src="https://picsum.photos/700/700" alt="" class="about-img" />
+				</div>
+				<div id="about-img-background-2">
+					<img src="https://picsum.photos/750/750" alt="" class="about-img" />
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Intro section 1 -->
+	<section id="contact-us-container">
+		<div class="decor-square-container">
+			<div class="decor-square-1 decor-square" />
+			<div class="decor-square-2 decor-square" />
+		</div>
+
+		<div class="intro-content">
+			<h1 class="intro-title">Find what matters to you. Fast.</h1>
+			<p class="intro-desc">
+				With Entree, lorem ispum dolar sit amet. Lorem ispum dolar sit amet Lorem ispum dolar sit
+				amet Lorem ispum dolar sit amet Lorem ispum dolar sit amet Lorem ispum dolar sit amet Lorem
+				ispum dolar sit amet Lorem ispum dolar sit amet
+			</p>
+		</div>
+	</section>
+
+	<!-- Intro section 2 -->
+	<section id="intro-container-2">
+		<div class="decor-square-container">
+			<div class="decor-square-1 decor-square" />
+			<div class="decor-square-2 decor-square" />
+			<div class="decor-square-3 decor-square" />
+			<div class="decor-square-4 decor-square" />
+			<div class="decor-square-5 decor-square" />
+		</div>
+		<div class="intro-content">
+			<h1 class="intro-title">Startups made more accessible than ever before.</h1>
+			<p class="intro-desc">
+				With Entre, lorem ispum dolar sit amet. Lorem ispum dolar sit amet Lorem ispum dolar sit
+				amet Lorem ispum dolar sit amet Lorem ispum dolar sit amet Lorem ispum dolar sit amet Lorem
+				ispum dolar sit amet Lorem ispum dolar sit amet Lorem ispum dolar sit amet Lorem ispum dolar
+				sit amet Lorem ispum dolar sit amet
+			</p>
+		</div>
+	</section>
+
+	<!-- Invite section -->
+	<section id="invite-container">
+		<div id="invite-content-container">
+			<h1 id="invite-title">What are you waiting for?</h1>
+		</div>
+		<div id="invite-btn-container">
+			<div id="invite-btn-background">
+				<div id="invite-btn-title-container">
+					<h1 id="invite-btn-title">Start now.</h1>
+				</div>
+				<div id="signup-btn-container">
+					<button class="signup-btn">Signup</button>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
